9f61700059ca7584aae9c9b82f4da558266e76af
Joe Garcia <joe@joe-garcia.com>
2019-12-26 17:03:12 -0500

    Added GET & POST tests

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify, request
+
+import boto3
 
 
 app = Flask(__name__)
@@ -6,3 +10,42 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/users/<string:user_id>")
+def get_user(user_id):
+    resp = client.get_item(
+        TableName=USERS_TABLE,
+        Key={
+            'userId': { 'S': user_id }
+        }
+    )
+    item = resp.get('Item')
+    if not item:
+        return jsonify({'error': 'User does not exist'}), 404
+
+    return jsonify({
+        'userId': item.get('userId').get('S')
+        'name': item.get('name').get('S')
+    })
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    user_id = request.json.get('userId')
+    name = request.json.get('name')
+    if not user_id or not name:
+        return jsonify({'error': 'Please provide userId and name'}), 400
+
+    resp = client.put.item(
+        TableName=USERS_TABLE,
+        Item={
+            'userId': {'S': user_id},
+            'name': {'S': name}
+        }
+    )
+
+    return jsonify({
+        'userId': user_id,
+        'name': name
+    })
